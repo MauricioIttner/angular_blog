5690327df0b112675f3ad97d33f26b3ec2531880
Mauricio Queiroz <mauricioittner30@gmail.com>
2023-09-19 19:13:45 -0300

    Components Dinamycs Properties

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = 'Cavaleiro Da Lua, Nova serie da disney+';
 
+  ngOnInit(): void {
+  }
 }
